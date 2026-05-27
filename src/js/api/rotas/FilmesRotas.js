1252e118bf4fea4f3bf0451d13f4ee9939daa4c9
Gabrielc476 <gabrielc.cavalcante@hotmail.com>
2026-05-27 11:49:19 -0300

    fix: route GET /filmes/seed to seed method and bypass authMiddleware

--- src/js/api/rotas/FilmesRotas.js
+++ src/js/api/rotas/FilmesRotas.js
@@ -7,8 +7,10 @@ const filmesController = new FilmesController();
 
 // Como no server.js usamos "app.use('/filmes', filmesRoutes)", 
 // a rota "/" aqui dentro já representa nativamente "/filmes".
+filmesRotas.get('/seed', (req, res) => filmesController.seed(req, res));
+filmesRotas.post('/seed', (req, res) => filmesController.seed(req, res));
+
 filmesRotas.get('/', authMiddleware, (req, res) => filmesController.listar(req, res));
 filmesRotas.get('/:id', authMiddleware, (req, res) => filmesController.detalhar(req, res));
-filmesRotas.post('/seed', (req, res) => filmesController.seed(req, res));
 
 export default filmesRotas;
